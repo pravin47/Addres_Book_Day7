f7244987b0171ab4845c46fa46fe1757147ce93f
pravin47 <bagul.pravin2@gmail.com>
2021-02-10 22:47:28 +0530

    Added:Ability_To_Edit_Contact

--- AddressBook.java
+++ AddressBook.java
@@ -48,6 +48,82 @@ class CreateContact {
 		System.out.println(book);
 	}
 
+	public static void editContact() {
+
+		System.out.println("Enter The First Name You Want To Edit");
+		String edit = sc.next();
+		boolean check = book.contains(NewContact.fName);
+		if (check == true) {
+			System.out.println("Please Enter Which Field You Want To Edit");
+			System.out.println("1 For New First Name");
+			System.out.println("2 For New Last Name");
+			System.out.println("3 For New Address");
+			System.out.println("4 For New City");
+			System.out.println("5 For New State");
+			System.out.println("6 For New Zip Code");
+			System.out.println("7 For New Phone Number");
+			System.out.println("8 For New Email Id");
+			int ch = sc.nextInt();
+			switch (ch) {
+
+			case 1:
+				System.out.println("Enter The New First Name");
+				String nName = sc.next();
+				book.set(0, nName);
+				System.out.println("First Name Update Successfully");
+				break;
+			case 2:
+				System.out.println("Enter The New Last Name");
+				String lName = sc.next();
+				book.set(1, lName);
+				System.out.println("Last Name Update Successfully");
+				break;
+
+			case 3:
+				System.out.println("Enter The New Address");
+				String nAddress = sc.next();
+				book.set(2, nAddress);
+				System.out.println("Address Update Successfully");
+				break;
+			case 4:
+				System.out.println("Enter The New City");
+				String nCity = sc.next();
+				book.set(3, nCity);
+				System.out.println("City Update Successfully");
+				break;
+			case 5:
+				System.out.println("Enter the New State");
+				String nState = sc.next();
+				book.set(4, nState);
+				System.out.println("State Update Successfully");
+				break;
+			case 6:
+				System.out.println("Enter The New Zip Code");
+				int nZip = sc.nextInt();
+				book.set(5, nZip);
+				System.out.println("Zip Code Update Successfully");
+				break;
+
+			case 7:
+				System.out.println("Enter The New Phone Number");
+				int nPh = sc.nextInt();
+				book.set(6, nPh);
+				System.out.println("Phone Number Update Successfully");
+				break;
+
+			case 8:
+				System.out.println("Enter The New Email Address");
+				String nEmail = sc.next();
+				book.set(7, nEmail);
+				System.out.println("Email Update Successfully");
+				break;
+
+			}
+
+		}
+
+	}
+
 }
 
 public class AddressBook extends CreateContact {
@@ -60,7 +136,8 @@ public class AddressBook extends CreateContact {
 			System.out.println("Please Enter Your Choice");
 			System.out.println("1 Add Contact");
 			System.out.println("2 Show The All Contact");
-			System.out.println("3 Exit");
+			System.out.println("3 For Edit Contact");
+			System.out.println("4 Exit");
 			int choice = sc.nextInt();
 			switch (choice) {
 
@@ -71,8 +148,12 @@ public class AddressBook extends CreateContact {
 			case 2:
 				show();
 				break;
-			
+
 			case 3:
+				editContact();
+				break;
+
+			case 4:
 				ans = false;
 
 			}
@@ -92,7 +173,8 @@ class NewContact {
 	static int phoneNumber;
 	static String email;
 
-	NewContact(String fName, String lName, String address, String city, String state, int zip, int phoneNumber,String email) {
+	NewContact(String fName, String lName, String address, String city, String state, int zip, int phoneNumber,
+			String email) {
 
 		this.fName = fName;
 		this.lName = lName;
